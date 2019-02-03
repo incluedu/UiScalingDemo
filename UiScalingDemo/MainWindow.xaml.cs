0fd7d90b9f3c42b45bb1229499ff2222cef28c54
Patric Hollenstein <patric.hollenstein@lustenauer.net>
2019-02-03 19:54:22 +0100

    Initial commit

---
+++ UiScalingDemo/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace UiScalingDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UiScaleSlider_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ((Slider) sender).Value = 1.0;
+        }
+    }
+}
